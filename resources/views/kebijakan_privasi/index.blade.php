dc3035f638f2c5db96c1c27ec63471274cd49dd0
anindasulistya28 <anindasulistya@gmail.com>
2022-09-13 09:16:35 +0700

    admin

--- resources/views/kebijakan_privasi/index.blade.php
+++ resources/views/kebijakan_privasi/index.blade.php
@@ -1,89 +1,96 @@
 <div>
-    <a href="#" data-toggle="modal" data-target="#ketentuan">
-        <h5 class="d-flex justify-content-between">
-            <i class="fa fa-user-secret" aria-hidden="true"></i>
-            Kebijakan Privasi
-            <a href="" data-toggle="modal" data-target="#ketentuan"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-        </h5>
-    </a>
-    <hr>
+  <a href="#" data-toggle="modal" data-target="#kebijakanPrivasi">
+      <h5 class="d-flex justify-content-between">
+          <i class="fa fa-user-secret" aria-hidden="true"></i>
+          Kebijakan Privasi
+          <a href="" data-toggle="modal" data-target="#kebijakanPrivasi"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+      </h5>
+  </a>
+  <hr>
 </div>
 
-<div class="modal fade" id="ketentuan" tabindex="-1" aria-labelledby="ketentuanLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content" style="border-radius: 20px">
-        <div class="modal-header">
-          <h4 class="modal-title m-3" id="ketentuanLabel">Kebijakan Privasi</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
+<div class="modal fade" id="kebijakanPrivasi" tabindex="-1" aria-labelledby="kebijakanPrivasiLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content" style="border-radius: 20px">
+      <div class="modal-header">
+        <h4 class="modal-title m-3" id="kebijakanPrivasiLabel">Kebijakan Privasi</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="m-3">
+          <div class="text-center">
+              <h3 class="m-2">PRIVACY POLICY</h3>
+              <p class="card-subtitle mt-2 text-muted m-2">(KETENTUAN PRIVASI)</p>
+              <br>
+          </div>
           <div class="m-3">
-            <div class="text-center">
-                <h3 class="m-2">PRIVACY POLICY</h3>
-                <p class="card-subtitle mt-2 text-muted m-2">(KEBIJAKAN PRIVASI)</p>
-                <br>
-            </div>
-            <div class="m-3">
-                <p>Ketentuan privasi ini mengungkapkan kebijakan penanganan data-data pribadi pengguna pada situs Kiddo.id milik PT Kebaikan Anak Indonesia.</p>
-            </div>
-            <div class="accordion" id="accordionExample">
-              <div class="card">
-                <div class="card-header" id="headingOne">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      <strong></strong>
-                    </button>
-                  </h2>
-                </div>
-            
-                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                  <div class="card-body">
-                    </div>
+              <p>Ketentuan privasi ini mengungkapkan kebijakan penanganan data-data pribadi pengguna pada situs Kiddo.id milik PT Kebaikan Anak Indonesia.</p>
+          </div>
+          <div class="accordion" id="accordionExample">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                  <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>Informasi yang kami ambil dari pengguna</strong>
+                  </button>
+                </h2>
+              </div>
+          
+              <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                  <ul>
+                    <li>Kiddo.id adalah merek dagang dari PT Kebaikan Anak Indonesia.</li>
+                    <li>Platform adalah tempat berlangsungnya aktivitas antara Kiddo.id dan penggunanya, seperti namun tidak terbatas pada situs web, aplikasi, dan lainnya.</li>
+                    <li>KiddoPoint adalah poin loyalitas dari Kiddo.id yang diberikan kepada para pengguna dengan syarat dan ketentuan yang berlaku.</li>
+                    <li>Produk dan Layanan adalah semua barang dan aktivitas yang dijual di dalam platform Kiddo.id.</li>
+                    <li>Penyedia Aktivitas adalah mitra Kiddo.id dalam menghadirkan Produk dan Layanan untuk pada pengguna.</li>
+                  </ul>
                 </div>
               </div>
-              <div class="card">
-                <div class="card-header" id="headingTwo">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      Collapsible Group Item #2
-                    </button>
-                  </h2>
-                </div>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                  <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                  </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Collapsible Group Item #2
+                  </button>
+                </h2>
+              </div>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                 </div>
               </div>
-              <div class="card">
-                <div class="card-header" id="headingThree">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Collapsible Group Item #3
-                    </button>
-                  </h2>
-                </div>
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                  <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                  </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Collapsible Group Item #3
+                  </button>
+                </h2>
+              </div>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-          <button type="button" class="btn btn-primary">Submit</button>
-        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Submit</button>
       </div>
     </div>
+  </div>
 </div>
 
 <script>
-  $(document).on('click', function(){
-    $('.collapse').collapse('hide');
-  })
+$(document).on('click', function(){
+  $('.collapse').collapse('hide');
+})
 </script>
